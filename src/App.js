4258b80b09081b979f0e9c12674dc77d914aeb3a
Jericho Faderon <jerichofaderon@gmai.com>
2023-03-01 18:43:51 -0500

    final commit, I will become a React pro

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
         <p> My name is Jericho and I'm going to master React!</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/jerichofaderon"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          "Check Me Out!"
         </a>
       </header>
     </div>
